4cc0ed1a2441cb72411413d6964da620fd32f1d3
wolfram0407 <wolfram@kakao.com>
2023-10-19 21:01:29 +0900

    First3

--- src/drawItems.js
+++ src/drawItems.js
@@ -2,13 +2,11 @@ const baseurl = "https://image.tmdb.org/t/p/w500";
 const drawItems = (movie) => {
   const imageUrl = baseurl + movie.poster_path;
   const url = new URL(window.location.href).host;
-  console.log(url);
-  const localurl = `${url}/detail.html?id=`;
-  console.log(localurl);
+  const urls = "http://" + url + "/detail.html?id=" + movie.id;
   let name = movie.title ? movie.title : movie.original_title;
   let date = movie.first_air_date ? movie.first_air_date : movie.release_date;
   let text = `  
-      <a class="movie" href="${localurl}${movie.id}" style="background-image:url(${imageUrl})">
+      <a class="movie" href="${urls}" style="background-image:url(${imageUrl})">
         <div class="info">
           <div class="title">${name}</div>
           <div class="year">${date}</div>
