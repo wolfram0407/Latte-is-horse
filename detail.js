7a45b0a4174724d2ca13e9c941ce5cfa15a93954
nugabah <cxcx0917@gmail.com>
2023-10-27 21:25:42 +0900

    리뷰 입력에 유효성검사 적용

--- detail.js
+++ detail.js
@@ -31,27 +31,27 @@ document.getElementById("reviewBtn").addEventListener("click", async function ()
     pw: password.value,
     date: new Date().getTime()
   };
-  // if (!Boolean(reviewer) || !Boolean(reviewcontent) || !Boolean(password)) {
-  //   alert("빈칸을 모두 입력해주세요!");
-  // } else if (typeof password !== 'number') {
-  //   console.log(typeof password);
-  //   alert("비밀번호는 숫자만 입력 가능합니다!"); //비밀번호칸의 숫자가 문자열로 들어감
-  // } else if (password.length < 4) {
-  //   alert("비밀번호는 4자리 이상입니다!");
-  // } else {
+  if (!Boolean(reviewer.value) || !Boolean(reviewcontent.value) || !Boolean(password.value)) {
+    alert("빈칸을 모두 입력해주세요!");
+  } else if (!Boolean(parseInt(password.value))) {
+    alert("비밀번호는 숫자만 입력 가능합니다!"); //비밀번호칸의 숫자가 문자열로 들어감
+  } else if (password.value.length < 4) {
+    alert("비밀번호는 4자리 이상입니다!");
+  } else {
     if (Boolean(parsingdata)) {
       reviewdata.push(...parsingdata);
     };
     reviewdata.push(userInfo);
     localStorage.setItem(`${getMovie.id}`, JSON.stringify(reviewdata));
     alert("리뷰가 작성되었습니다");
-
+    console.log(password.value.length);
     // reviewer.value = "";
     // password.value = "";
     // reviewcontent.value = "";
 
     window.location.reload();
-  // }
+    // }
+  }
 });
 
 function displayReviews() {
