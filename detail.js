8b1eafdc0f1f85dbb846927bcdcb8096d6cd15cc
asdfg20564 <44521546+asdfg20564@users.noreply.github.com>
2023-10-27 10:54:36 +0900

    make validation
    make validation logic

--- detail.js
+++ detail.js
@@ -18,38 +18,7 @@ back.addEventListener("click", () => {
   history.go(-1);
 });
 
-const reviewValidation = async (review) => {
-  let check = true;
-  const badwords = ["바보", "멍청이", "시발", "머저리", "새끼"];
-  let badcheck = true;
-  badwords.forEach((element) => {
-    if (review.context.includes(element) === true) {
-      badcheck = false;
-    }
-  });
-
-  console.log(Number("a111")); //NaN
-
-  if (review.name === "" || review.context === "" || review.password === "") {
-    alert(`내용을 모두 입력해주세요!`);
-    check = false;
-  } else if (
-    review.password.length !== 4 ||
-    parseInt(review.password).length !== 4
-  ) {
-    alert(`비밀번호는 4자리 숫자입니다!`);
-    check = false;
-  } else if (review.context.length < 20 || review.context.length > 400) {
-    alert(`리뷰는 20자 이상 400자 이하로 작성 부탁드립니다.`);
-    check = false;
-  } else if (badcheck === false) {
-    alert(`욕은 작성할 수 없습니다.`);
-    check = false;
-  }
-
-  return check;
-};
-
+/*
 reviewsubmit.addEventListener("click", async () => {
   let username = document.querySelector(".review-name").value;
   let usercontext = document.querySelector(".review-context").value;
@@ -77,7 +46,7 @@ reviewsubmit.addEventListener("click", async () => {
   let newz = await findByOneDetail(urlParams);
   window.location.reload(true);
 });
-
+*/
 if (reviewremove) {
   reviewremove.addEventListener("click", async () => {
     let keynum = 1;
