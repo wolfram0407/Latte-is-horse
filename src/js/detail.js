b298d736b2bd725bc18dc9a3fb7118f2664051f1
asdfg20564 <44521546+asdfg20564@users.noreply.github.com>
2023-10-30 12:37:33 +0900

    Update detail.js
    update detail.js

--- src/js/detail.js
+++ src/js/detail.js
@@ -25,29 +25,31 @@ drawItem_detail(getMovie);
 
 const parsingdata = JSON.parse(localStorage.getItem(`${getMovie.id}`));
 // 등록
-document.getElementById("reviewBtn").addEventListener("click", async function () {
-  let reviewdata = [];
-  let reviewer = document.getElementById("userName");
-  let reviewcontent = document.getElementById("reviewText");
-  let password = document.getElementById("password");
-  let userInfo = {
-    user: reviewer.value,
-    review: reviewcontent.value,
-    pw: password.value,
-    date: new Date().getTime(),
-  };
+document
+  .getElementById("reviewBtn")
+  .addEventListener("click", async function () {
+    let reviewdata = [];
+    let reviewer = document.getElementById("userName");
+    let reviewcontent = document.getElementById("reviewText");
+    let password = document.getElementById("password");
+    let userInfo = {
+      user: reviewer.value,
+      review: reviewcontent.value,
+      pw: password.value,
+      date: new Date().getTime(),
+    };
 
-  if (reviewValidation(userInfo)) {
-    //처음 parsingdata를 가져올때 key가 없으면 null을 넣기에 null을 제외하는 if문
-    if (parsingdata) {
-      reviewdata.push(...parsingdata);
+    if (reviewValidation(userInfo)) {
+      //처음 parsingdata를 가져올때 key가 없으면 null을 넣기에 null을 제외하는 if문
+      if (parsingdata) {
+        reviewdata.push(...parsingdata);
+      }
+      reviewdata.push(userInfo);
+      localStorage.setItem(`${getMovie.id}`, JSON.stringify(reviewdata));
+      alert("리뷰가 작성되었습니다.");
+      window.location.reload();
     }
-    reviewdata.push(userInfo);
-    localStorage.setItem(`${getMovie.id}`, JSON.stringify(reviewdata));
-    alert("리뷰가 작성되었습니다");
-    window.location.reload();
-  }
-});
+  });
 
 displayReviews(parsingdata);
 
@@ -76,7 +78,7 @@ document.querySelectorAll(".removeBtn").forEach((btn) =>
           //빈 배열이 아니라면 다시 setItem으로 남은값을 저장
           localStorage.setItem(`${getMovie.id}`, JSON.stringify(leftdata));
         }
-        alert("삭제 완료");
+        alert("삭제 완료!");
         window.location.reload();
       } else {
         alert("비밀번호가 일치하지 않습니다.");
@@ -91,7 +93,10 @@ document.querySelectorAll(".removeBtn").forEach((btn) =>
 document.querySelectorAll(".editBtn").forEach((btn) =>
   btn.addEventListener("click", (event) => {
     console.log(event);
-    const reviewIndex = event.target.parentElement.parentElement.parentElement.getAttribute("reviewIndex");
+    const reviewIndex =
+      event.target.parentElement.parentElement.parentElement.getAttribute(
+        "reviewIndex"
+      );
     const inputpw = prompt("비밀번호를 입력해주세요.");
 
     if (inputpw) {
