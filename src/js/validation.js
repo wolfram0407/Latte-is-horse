4934d27e28ba3f1db871b47ab709ccf5b255732c
nugabah <cxcx0917@gmail.com>
2023-10-30 14:53:55 +0900

    리뷰 수정내용에 유효성검사추가

--- src/js/validation.js
+++ src/js/validation.js
@@ -55,4 +55,13 @@ const isCheckBadWord = (review) => {
   return isWord;
 };
 
-export { reviewValidation, isComparePassword, isCheckBadWord };
+const editValidation = (editData) => {
+  let check = true;
+  if (editData.length < 20 || editData.length > 400) {
+  alert(`리뷰는 20자 이상 400자 이하로 작성 부탁드립니다.`);
+  check = false;
+  }
+  return check;
+};
+
+export { reviewValidation, isComparePassword, isCheckBadWord, editValidation};
